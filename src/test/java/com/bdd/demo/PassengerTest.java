2ac5a3514d46e70cd959579ed4d53f1a8fd404e6
Dimitrije Dragicevic <dimitrije.dragicevic@panonit.com>
2021-10-29 10:00:56 +0200

    added tests and junit vintage

--- src/test/java/com/bdd/demo/PassengerTest.java
+++ src/test/java/com/bdd/demo/PassengerTest.java
@@ -1,27 +1,41 @@
 package com.bdd.demo;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import com.bdd.demo.airport.EconomyFlight;
+import com.bdd.demo.airport.Flight;
+import com.bdd.demo.airport.Passenger;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class PassengerTest {
 
+	private Flight economyFlight;
+	private Passenger passenger;
+
 	@Given("there is an economy flight")
 	public void there_is_an_economy_flight() {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
+		economyFlight = new EconomyFlight("1");
 	}
 
-	@When("we have a usual passenger")
+	@When("^we have a usual passenger")
 	public void we_have_a_usual_passenger() {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
+		passenger = new Passenger("Mike", false);
 	}
 
-	@Then("you can add and remove him from an economy flight")
+	@Then("^you can add and remove him from an economy flight")
 	public void you_can_add_and_remove_him_from_an_economy_flight() {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
+		assertAll(() -> assertEquals("1", economyFlight.getId()),
+				() -> assertEquals(true, economyFlight.addPassenger(passenger)),
+				() -> assertEquals(1, economyFlight.getPassengersList().size()),
+				() -> assertEquals("Mike", economyFlight.getPassengersList().get(0).getName()),
+				() -> assertEquals(true, economyFlight.removePassenger(passenger)),
+				() -> assertEquals(0, economyFlight.getPassengersList().size())
+
+		);
 	}
 
 	@When("we have a VIP passenger")
