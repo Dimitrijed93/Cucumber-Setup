1047f278a3a3fc1c4cc18d31ae37605e0f93a2c6
Dimitrije Dragicevic <dimitrije.dragicevic@panonit.com>
2021-10-29 08:21:26 +0200

    setup

---
+++ src/test/java/com/bdd/demo/CucumberTest.java
@@ -0,0 +1,12 @@
+package com.bdd.demo;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources")
+public class CucumberTest {
+
+}
---
+++ src/test/java/com/bdd/demo/PassengerTest.java
@@ -0,0 +1,57 @@
+package com.bdd.demo;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class PassengerTest {
+
+	@Given("there is an economy flight")
+	public void there_is_an_economy_flight() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+
+	@When("we have a usual passenger")
+	public void we_have_a_usual_passenger() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("you can add and remove him from an economy flight")
+	public void you_can_add_and_remove_him_from_an_economy_flight() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+
+	@When("we have a VIP passenger")
+	public void we_have_a_vip_passenger() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("you can add him but cannot remove him from an economy flight")
+	public void you_can_add_him_but_cannot_remove_him_from_an_economy_flight() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("there is an business flight")
+	public void there_is_an_business_flight() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("you cannot add or remove him from a business flight")
+	public void you_cannot_add_or_remove_him_from_a_business_flight() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("you can add him but cannot remove him from a business flight")
+	public void you_can_add_him_but_cannot_remove_him_from_a_business_flight() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+
+}
